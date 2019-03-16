105b79d73b99130817f5d42bc2c61dd0913ce807
Vadim <vkravchik@gmail.com>
2019-03-16 22:46:53 +0200

    choto tam sdelal

--- src/app/page/user/user.component.ts
+++ src/app/page/user/user.component.ts
@@ -15,7 +15,6 @@ export class UserComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'email', 'password'];
   users = new MatTableDataSource<User>();
-  // users: User[];
   newUser = {
     email: null,
     password: null
