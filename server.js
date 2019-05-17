c2965b5279a568064deb438239c38109782eda52
Vadim <vkravchik@gmail.com>
2019-05-17 12:21:55 +0300

    small fix

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/DB'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname,'/dist/DB/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
